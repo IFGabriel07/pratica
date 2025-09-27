a4875e86b088cc9ec3f6627acf4b8e09d040510c
IFGabriel07 <gfss1@aluno.ifal.edu.br>
2025-09-26 22:35:25 -0300

    chore: add Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-
+import Header from "@/app/_componentes/header";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 export default function Home() {
+
   return (
     <div>
-     <Button className= "border-purple-500 text-purple-500 hover:bg-purple-500 hover:text-white" variant="outline">Button</Button>
+      <Header />
+
+      <div className="p-5">
+        <p className="text-x1 font-bold">Olá,Meu Rato</p>
+        <p className="text-sm">Quarta Feira, Dia 24 de dezembro</p>
+      </div>
+
+      <div className="flex flex-row item-center gap-4 px-5">
+        <Input placeholder=" Buscar Serviços" />
+        <Button size={"icon"}>
+          <SearchIcon />
+
+        </Button>
+      </div>
+
     </div>
   );
 }
